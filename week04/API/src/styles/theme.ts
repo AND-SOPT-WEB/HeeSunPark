137ce80d3816f48d63f77d7fdd681c43cb1cc13e
Heesun <heesun729@naver.com>
2024-11-12 13:01:40 +0900

    feat: 전역 style 파일

---
+++ week04/API/src/styles/theme.ts
@@ -0,0 +1,18 @@
+import { DefaultTheme } from 'styled-components';
+import colors from './colors';
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: colors.softblue,
+    secondary: colors.mediumblue,
+    textPrimary: colors.black,
+    textSecondary: colors.darkgray,
+    gray: colors.gray,
+    white: colors.white,
+    blue: colors.blue,
+    darkblue: colors.darkblue,
+    softblue: colors.softblue,
+  },
+};
+
+export default theme;
